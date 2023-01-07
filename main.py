12afcb7ab1710c328f82fedcff8655848a70d64c
Huseyn-Iskandarli <huseyn.iskandarli@gmail.com>
2023-01-07 11:10:49 +0400

    Modified for running via pm2

--- main.py
+++ main.py
@@ -13,54 +13,53 @@ db = cluster["test_db"]
 collection = db["test_col"]
 
 try: 
-	while True:
-		# LOGGING
-		with open("logs.txt", "a") as file:
-			file.write("\nStarted Scraping @ " + str(datetime.now(pytz.timezone("Asia/Baku"))))
-			
-		# All scraping will happen here =============================================
-		# Collect all the links to the properties	todays_properties = []
-		todays_properties = []
-		pages = range(450)
-		for i in pages:
-			soup = requestSample("https://bina.az/alqi-satqi?page=" + str(i + 1))
-			for x in soup.find_all("div", class_="items-i"):
-				# if not posted today
-				if not x.find("div", class_="city_when").text.replace(" ", "").split(",")[1][:5] == "dünən":
-					# ignore those ads not posted today
-					continue
-				# collect the links from the ads posted today
-				else:
-					link = "https://" + "bina.az/" + x.find("a", class_="item_link").get('href')
-					todays_properties.append(link)
-			print("Page " + str(i+1) + " DONE")
-		print(str(len(todays_properties)) + "properties")
-		# Scrap from the individual ad pages ========================================
-		z = 1
-		for url in todays_properties:
-			try: 
-				print("Proprty #: " + str(z), url)
-				x =	page_scraper(url)
-				x["link"] = url
-				collection.insert_one(x)
-				print(x["category"], x["price"], x["currency"], x["link"])
-				z += 1
-			except Exception as e:
-				with open("logs.txt","a") as file:
-					file.write("\nPage Error @ " + str(datetime.now(pytz.timezone("Asia/Baku"))) + " " + str(e) + x["link"])
+	# LOGGING
+	with open("logs.txt", "a") as file:
+		file.write("\nStarted Scraping @ " + str(datetime.now(pytz.timezone("Asia/Baku"))))
+		
+	# All scraping will happen here =============================================
+	# Collect all the links to the properties	todays_properties = []
+	todays_properties = []
+	pages = range(450)
+	for i in pages:
+		soup = requestSample("https://bina.az/alqi-satqi?page=" + str(i + 1))
+		for x in soup.find_all("div", class_="items-i"):
+			# if not posted today
+			if not x.find("div", class_="city_when").text.replace(" ", "").split(",")[1][:5] == "dünən":
+				# ignore those ads not posted today
 				continue
-			
-		# All scraping ends here
-		print("=== Action Complete ! ===")	
+			# collect the links from the ads posted today
+			else:
+				link = "https://" + "bina.az/" + x.find("a", class_="item_link").get('href')
+				todays_properties.append(link)
+		print("Page " + str(i+1) + " DONE")
+	print(str(len(todays_properties)) + "properties")
+	
+	# LOGGING
+	with open("logs.txt","a") as file:
+		file.write("\nProperties to be scraped: " + str(len(todays_properties)))
+ 
+ 	# Scrap from the individual ad pages ========================================
+	z = 1
+	for url in todays_properties:
+		try: 
+			print("Proprty #: " + str(z), url)
+			x =	page_scraper(url)
+			x["link"] = url
+			collection.insert_one(x)
+			print(x["category"], x["price"], x["currency"], x["link"])
+			z += 1
+		except Exception as e:
+			with open("logs.txt","a") as file:
+				file.write("\nPage Error @ " + str(datetime.now(pytz.timezone("Asia/Baku"))) + " " + str(e) + x["link"])
+			continue
+		
+	# All scraping ends here
+	print("=== Action Complete ! ===")	
 
-		# LOGGING
-		with open("logs.txt","a") as file:
-		    file.write("\nDone Scraping @ " + str(datetime.now(pytz.timezone("Asia/Baku"))) + " // " + "Ads Scraped: " + str(len(todays_properties)))
-
-		# repeat_in = 24*3600
-		# for i in range(repeat_in):
-		# 	print("Time since last scan: ",round((i-i%3600)/3600), " h ", round((i%3600-i%3600%60)/60)," m ", i%60, " s")
-		# 	time.sleep(1)
+	# LOGGING
+	with open("logs.txt","a") as file:
+		file.write("\nDone Scraping @ " + str(datetime.now(pytz.timezone("Asia/Baku"))) + " // " + " Ads Scraped: " + str(len(todays_properties)))
 
 except Exception as e:
 	exc_type, exc_obj, exc_tb = sys.exc_info()
